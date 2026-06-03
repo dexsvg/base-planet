6adaf844f140ec5bab596245f66403ebed72df79
dexsvg <mdedi091@gmail.com>
2026-06-04 00:22:55 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,22 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import { ThirdwebProvider, coinbaseWallet, metamaskWallet, okxWallet } from "@thirdweb-dev/react";
+import { Base } from "@thirdweb-dev/chains";
+import Head from "next/head";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider
+      activeChain={Base}
+      clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
+      supportedWallets={[
+        coinbaseWallet({ recommended: true }),
+        okxWallet(),
+        metamaskWallet(),
+      ]}
+    >
+      <Head>
+        <meta name="google" content="notranslate" />
+      </Head>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }

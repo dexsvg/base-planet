4d2510a0b9e227e986b5b33b1ffb7cf7032ab8f9
dexsvg <mdedi091@gmail.com>
2026-06-03 19:26:47 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@ import Head from "next/head";
 
 const PROPERTIES = [
   { id: 1, name: "Tropical Exo-Planet", sector: "Primary Systems", specs: "Beds | Baths | Sq10 Sq Ft | Habitable Biomes", price: "0.15 ETH", tag: "FEATURED", image: "/assets/exo-planet.jpg" },
-  { id: 2, name: "Cyberpunk Space Station", sector: "Primary Systems", specs: "Beds | Baths | SqFt Sq Ft | Primary Systems", price: "2.5k ETH", tag: "LUXURY", image: "/assets/Station.jpg" },
-  { id: 3, name: "Luxury Starship Hangar", sector: "Primary Systems", specs: "Beds | Baths | Sq10 Sq Ft | Surface Area (km²)", price: "0.25 ETH", tag: "LIQUID", image: "/assets/Hangar.jpeg" },
+  { id: 2, name: "Cyberpunk Space Station", sector: "Primary Systems", specs: "Beds | Baths | SqFt Sq Ft | Primary Systems", price: "2.5k ETH", tag: "LUXURY", image: "/assets/station.jpg" },
+  { id: 3, name: "Luxury Starship Hangar", sector: "Primary Systems", specs: "Beds | Baths | Sq10 Sq Ft | Surface Area (km²)", price: "0.25 ETH", tag: "LIQUID", image: "/assets/sangar.jpeg" },
   { id: 4, name: "Family Home For Space", sector: "Primary Systems", specs: "Beds | Baths | Sq10 Sq Ft | Family Biome", price: "25k ETH", tag: "LOUNGE", image: "/assets/exo-planet.jpg" }, // Sementara pakai exo-planet karena file family-home belum ada
-  { id: 5, name: "Gangeoses-Instrose Plot", sector: "Primary Systems", specs: "Beds | Baths | SqFt Sq Ft | Surface Area (km²)", price: "2.5k ETH", tag: "LUXURE", image: "/assets/Plot.jpeg" },
+  { id: 5, name: "Gangeoses-Instrose Plot", sector: "Primary Systems", specs: "Beds | Baths | SqFt Sq Ft | Surface Area (km²)", price: "2.5k ETH", tag: "LUXURE", image: "/assets/plot.jpeg" },
   { id: 6, name: "Sprawling Ice Planet", sector: "Primary Systems", specs: "Beds | Bath | SqF5 Sq Ft | Asset Type", price: "2.5k ETH", tag: "HYDRO", image: "/assets/ice-planet.jpg" }
 ];
 
